3467b9830882de5caf70e5daec3f7be71d7f49ea
4ard <112971420+4ard@users.noreply.github.com>
2023-05-04 18:50:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -48,9 +48,9 @@ foreach($superpowers as $checking){
 	}
 }
 
-$user = 'u52927';
-$pass = '5758562';
-$db = new PDO('mysql:host=localhost;dbname=u52927', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+$user = 'u52971';
+$pass = '6342896';
+$db = new PDO('mysql:host=localhost;dbname=u52971', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 try {
   $stmt = $db->prepare("INSERT INTO form SET name=:name, email=:email, year=:byear, pol=:pol, limbs=:limbs, bio=:bio");
   $stmt->bindParam(':name', $name);
